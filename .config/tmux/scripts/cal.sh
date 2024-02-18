fbecb94ae294d3e76c9020129e10f06723675879
Phanor Coll <phanorcoll@gmail.com>
2024-02-18 17:06:42 -0400

    initial dotfiles
    - tmux
    - zshrc
    - initial script for cal for Tmux

---
+++ .config/tmux/scripts/cal.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# https://hasseg.org/icalBuddy/
+# https://github.com/omerxx/dotfiles/tree/master/tmux
+# get the list of events from google cal
+# build a CLI that can do this and integrate it to this script
+# to display in TMUX
+NERD_FONT_FREE="󱁕 "
+NERD_FONT_MEETING="󰤙 "
+
+echo "$NERD_FONT_FREE"
---
+++ README.md
@@ -0,0 +1,6 @@
+# Configuration files
+
+## Install
+Clone the repo in any location and inside the root folder run
+
+`stow --target ~/ .`
