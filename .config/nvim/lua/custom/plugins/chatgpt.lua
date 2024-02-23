7184ed2e0c6607cfdec4b33ce82a2f36ec411e30
Phanor Coll <phanorcoll@gmail.com>
2024-02-23 09:20:45 -0400

    feat(nvim): add chatgpt.nvim

---
+++ .config/nvim/lua/custom/plugins/chatgpt.lua
@@ -0,0 +1,15 @@
+return {
+  "jackMort/ChatGPT.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("chatgpt").setup({
+      api_key_cmd = "op read op://code_keys/chatgpt.nvim key/credential --no-newline"
+    })
+  end,
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "folke/trouble.nvim",
+    "nvim-telescope/telescope.nvim"
+  }
+}
