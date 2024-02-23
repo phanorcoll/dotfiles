6c661c61500201d45c726f1dd322d218fbce3b46
Phanor Coll <phanorcoll@gmail.com>
2024-02-23 13:02:32 -0400

    chore(nvim): update chatgpt config
    - Remove config params, and left it to the default OPENAI_API_KEY env
      ariable.

--- .config/nvim/lua/custom/plugins/chatgpt.lua
+++ .config/nvim/lua/custom/plugins/chatgpt.lua
@@ -2,9 +2,7 @@ return {
   "jackMort/ChatGPT.nvim",
   event = "VeryLazy",
   config = function()
-    require("chatgpt").setup({
-      api_key_cmd = "op read op://code_keys/chatgpt.nvim key/credential --no-newline"
-    })
+    require("chatgpt").setup()
   end,
   dependencies = {
     "MunifTanjim/nui.nvim",
