c67838de82905a433394ebbf37ad8d8c679978b8
Phanor Coll <phanorcoll@gmail.com>
2024-02-27 10:02:22 -0400

    chore(lazygit): Move setup to its own config variable

--- .config/nvim/lua/custom/plugins/lazygit.lua
+++ .config/nvim/lua/custom/plugins/lazygit.lua
@@ -3,14 +3,16 @@ vim.keymap.set("n", "<leader>lg", "<cmd>LazyGit<CR>", {
 })
 return {
   "kdheepak/lazygit.nvim",
-  require("lazy").setup({
-    {
-      "kdheepak/lazygit.nvim",
-      -- optional for floating window border decoration
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-telescope/telescope.nvim",
+  config = function()
+    require("lazy").setup({
+      {
+        "kdheepak/lazygit.nvim",
+        -- optional for floating window border decoration
+        dependencies = {
+          "nvim-lua/plenary.nvim",
+          "nvim-telescope/telescope.nvim",
+        },
       },
-    },
-  })
+    })
+  end,
 }
