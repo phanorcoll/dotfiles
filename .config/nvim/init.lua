44a99cb7e3fe7b361fa77811b8b1774d5de4ad4e
Phanor Coll <phanorcoll@gmail.com>
2024-02-27 09:12:07 -0400

    chore(lualine): Add the branch component to the section

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -158,7 +158,7 @@ require('lazy').setup({
       },
       sections = {
         lualine_a = { 'mode' },
-        lualine_b = { 'diff', 'diagnostics' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
         lualine_c = { 'filename' },
         lualine_x = { 'encoding', 'filetype' },
         lualine_y = { 'progress' },
