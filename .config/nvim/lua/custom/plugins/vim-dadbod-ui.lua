138987454ac662ca76f2a24653d5211c278150e1
Phanor Coll <phanorcoll@gmail.com>
2024-02-27 10:22:56 -0400

    chore(dadbod ui): Change mapping doc

--- .config/nvim/lua/custom/plugins/vim-dadbod-ui.lua
+++ .config/nvim/lua/custom/plugins/vim-dadbod-ui.lua
@@ -1,6 +1,6 @@
 -- https://github.com/kristijanhusak/vim-dadbod-ui
 vim.keymap.set('n', '<leader>db', '<cmd>DBUIToggle<CR>', {
-  desc = "Toogle [D][B]UI",
+  desc = "Toogle [D]ad[B]odUI",
 })
 return {
   'kristijanhusak/vim-dadbod-ui',
