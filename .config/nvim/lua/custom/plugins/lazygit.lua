ed0198c7e949931f3a39ef6f29ac6f099ae8ce71
Phanor Coll <phanorcoll@gmail.com>
2024-03-01 12:26:22 -0400

    chore(lazygit): Update config file

--- .config/nvim/lua/custom/plugins/lazygit.lua
+++ .config/nvim/lua/custom/plugins/lazygit.lua
@@ -8,6 +8,13 @@ return {
       {
         "kdheepak/lazygit.nvim",
         -- optional for floating window border decoration
+        cmd = {
+          "LazyGit",
+          "LazyGitConfig",
+          "LazyGitCurrentFile",
+          "LazyGitFilter",
+          "LazyGitFilterCurrentFile",
+        },
         dependencies = {
           "nvim-lua/plenary.nvim",
           "nvim-telescope/telescope.nvim",
