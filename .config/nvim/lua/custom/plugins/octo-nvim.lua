5d0711166d0e30a9a93d26534bb1c9bce4457bfc
Phanor Coll <phanorcoll@gmail.com>
2024-03-01 11:05:13 -0400

    chore(octo): Update configuration

--- .config/nvim/lua/custom/plugins/octo-nvim.lua
+++ .config/nvim/lua/custom/plugins/octo-nvim.lua
@@ -9,8 +9,15 @@ return {
   },
   config = function()
     require "octo".setup({
-      enable_builtin = true
+      enable_builtin = true,
+      mappings = {
+        review_diff = {
+          select_next_entry = { lhs = "<Tab>", desc = "move to previous changed file" },
+          select_prev_entry = { lhs = "<S-Tab>", desc = "move to next changed file" },
+        }
+      }
     })
+    vim.treesitter.language.register("markdown", "octo")
     -- vim.cmd([[hi OctoEditable guibg=none]])
   end,
   keys = {
