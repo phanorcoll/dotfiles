46319d3b530312ba2de880a4f1abd1541ab6604f
Phanor Coll <phanorcoll@gmail.com>
2024-03-01 11:00:14 -0400

    chore(noice): Added notify options to remove bg error
    - Update noice configuration

--- .config/nvim/lua/custom/plugins/noice.lua
+++ .config/nvim/lua/custom/plugins/noice.lua
@@ -4,6 +4,23 @@ return {
   event = "VeryLazy",
   opts = {
     -- add any options here
+    ---@type NoicePresets
+    presets = { inc_rename = true },
+    ---@type NoiceConfigViews
+    views = {
+      cmdline_popup = {
+        position = {
+          row = 7,
+          col = "55%",
+        },
+      },
+      cmdline_popupmenu = {
+        position = {
+          row = 7,
+          col = "55%",
+        },
+      },
+    },
   },
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
@@ -11,6 +28,13 @@ return {
     -- OPTIONAL:
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
-    "rcarriga/nvim-notify",
+    {
+      "rcarriga/nvim-notify",
+      opts = {
+        background_colour = "#2E3440",
+        stages = "static",
+        timeout = 2000,
+      },
+    },
   }
 }
