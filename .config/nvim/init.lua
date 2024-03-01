f8755f2b8566a1e6426a4baab9e63ce13f4f0a46
Phanor Coll <phanorcoll@gmail.com>
2024-03-01 10:40:37 -0400

    chore(lualine): Move config to lualine.lua
    - Remove the background from the theme
    - Remove sections and content

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -146,36 +146,7 @@ require('lazy').setup({
   },
 
   {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = true,
-        theme = 'iceberg_dark',
-        component_separators = '|',
-        section_separators = { left = '', right = '' },
-      },
-      sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
-        lualine_x = { 'encoding', 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' }
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = { 'diagnostics' },
-        lualine_c = { 'filename' },
-        lualine_x = { 'location' },
-        lualine_y = {},
-        lualine_z = {}
-      },
-    },
-  },
-
-  {
+    -- TODO: remove indentation guides
     -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
     main = "ibl",
@@ -240,6 +211,7 @@ require('lazy').setup({
 -- Set highlight on search
 vim.o.hlsearch = true
 
+-- TODO: remove line number
 -- Make line numbers default
 vim.wo.number = true
 
