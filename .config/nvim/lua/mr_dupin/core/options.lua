ad4e1fad1d3e77538e39ab6b412bc2596afdd39a
Phanor Coll <phanorcoll@gmail.com>
2024-04-05 17:16:43 -0400

    chore: Set initial options

--- .config/nvim/lua/mr_dupin/core/options.lua
+++ .config/nvim/lua/mr_dupin/core/options.lua
@@ -4,10 +4,13 @@ local opt = vim.opt
 
 -- set termguicolors and visuals
 opt.termguicolors = true
-opt.background = "dark"
+-- opt.background = "dark"
 opt.signcolumn = "yes"
 opt.relativenumber = true
 opt.number = true
+
+--clipboard
+opt.clipboard:append("unnamedplus")
 
 -- tabs & indentation
 opt.tabstop = 2
@@ -15,8 +18,13 @@ opt.shiftwidth = 2
 opt.expandtab = true
 opt.autoindent = true
 opt.wrap = false
+opt.backspace = "indent,eol,start"
 
 -- search settings
 opt.ignorecase = true
 opt.smartcase = true
 opt.cursorline = true
+
+-- window splitting
+opt.splitright = true
+opt.splitbelow = true
