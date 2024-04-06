c4a5f3d82c24c9259b372d5f50aa2d2562600902
Phanor Coll <phanorcoll@gmail.com>
2024-04-06 19:52:48 -0400

    chore: Update configuration

--- .config/nvim/lua/mr_dupin/plugins/auto-session.lua
+++ .config/nvim/lua/mr_dupin/plugins/auto-session.lua
@@ -1,17 +1,19 @@
 -- https://github.com/rmagatti/auto-session
 -- TODO: Tweak config
-return{
-  "rmagatti/auto-session",
+return {
+  'rmagatti/auto-session',
   config = function()
-    local auto_session = require("auto-session")
+    local auto_session = require 'auto-session'
 
-    auto_session.setup({
+    auto_session.setup {
+      log_level = 'error',
       auto_restore_enabled = false,
-      auto_session_suppress_dirs = {"~/", "~/Downloads"},
-    })
+      auto_session_suppress_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+    }
 
     local keymap = vim.keymap
-    keymap.set("n","<leader>wr","<cmd>SessionRestore<CR>",{ desc = "Restore session for cwd"})
-    keymap.set("n","<leader>ws","<cmd>SessionSave<CR>",{ desc = "Save session"})
-  end
+    keymap.set('n', '<leader>wr', '<cmd>SessionRestore<CR>', { desc = 'Restore session for cwd' })
+    keymap.set('n', '<leader>ws', '<cmd>SessionSave<CR>', { desc = 'Save session' })
+    vim.o.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions'
+  end,
 }
