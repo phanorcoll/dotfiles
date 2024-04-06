4cab90d8d2e1ade7f4782efbc7e2b987984d699e
Phanor Coll <phanorcoll@gmail.com>
2024-04-06 17:34:19 -0400

    chore: Remove foldcolumn from nvim-ufo

--- .config/nvim/lua/mr_dupin/plugins/nvim-ufo.lua
+++ .config/nvim/lua/mr_dupin/plugins/nvim-ufo.lua
@@ -36,6 +36,7 @@ return {
       return newVirtText
     end
     require('ufo').setup {
+      close_fold_kinds_for_ft = {},
       enable_get_fold_virt_text = true,
       fold_virt_text_handler = handler,
       open_fold_hl_timeout = 150,
@@ -70,7 +71,7 @@ return {
         vim.lsp.buf.hover()
       end
     end)
-    vim.o.foldcolumn = '1' -- '0' is not bad
+    vim.o.foldcolumn = '0' -- '0' is not bad
     vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
     vim.o.foldlevelstart = 99
     vim.o.foldenable = true
