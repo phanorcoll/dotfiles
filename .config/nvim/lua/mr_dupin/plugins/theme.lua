4802977c1e67f82c03ace21b3e71bb4eadda6225
Phanor Coll <phanorcoll@gmail.com>
2024-04-06 19:02:18 -0400

    chore: Change Theme

--- .config/nvim/lua/mr_dupin/plugins/theme.lua
+++ .config/nvim/lua/mr_dupin/plugins/theme.lua
@@ -1,23 +1,11 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
+  'navarasu/onedark.nvim',
   priority = 1000,
   config = function()
-    require("tokyonight").setup({
-      style = "night",
+    require('onedark').setup {
+      style = 'cool',
       transparent = true,
-      styles = {
-        -- Style to be applied to different syntax groups
-        -- Value is any valid attr-list value for `:help nvim_set_hl`
-        comments = { italic = true },
-        keywords = { italic = true },
-        functions = {},
-        variables = { italic = true},
-        -- Background styles. Can be "dark", "transparent" or "normal"
-        sidebars = "dark", -- style for sidebars, see below
-        floats = "dark", -- style for floating windows
-      },
-    })
-    vim.cmd("colorscheme tokyonight-night")
-  end
+    }
+    vim.cmd.colorscheme 'onedark'
+  end,
 }
