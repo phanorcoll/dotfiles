71b4b55a7ccaa60de6ecc3c35c6e8ab6abb5b85c
Phanor Coll <phanorcoll@gmail.com>
2024-04-06 10:09:33 -0400

    chore: Add trouble package to list diagnostics

---
+++ .config/nvim/lua/mr_dupin/plugins/trouble.lua
@@ -0,0 +1,13 @@
+--
+return{
+  "folke/trouble.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
+  keys = {
+    {"<leader>xx", "<cmd>TroubleToggle<CR>", desc = "Open/close trouble list"},
+    {"<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Open workspace trouble"},
+    {"<leader>xd", "<cmd>TroubleToggle document_diagnostics<CR>", desc = "Open document trouble list"},
+    {"<leader>xq", "<cmd>TroubleToggle quickfix<CR>", desc = "Open trouble quickfix list"},
+    {"<leader>xl", "<cmd>TroubleToggle loclist<CR>", desc = "Open trouble location list"},
+    {"<leader>xt", "<cmd>TodoTrouble<CR>", desc = "Open TODOs in trouble"},
+  },
+}
