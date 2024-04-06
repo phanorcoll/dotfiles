67ed2e38d57de58266d096ff7020084e1c113fd4
Phanor Coll <phanorcoll@gmail.com>
2024-04-06 10:44:29 -0400

    chore: Add nvim-lint for linters with basic config and initial linters

--- .config/nvim/lua/mr_dupin/plugins/lsp/mason.lua
+++ .config/nvim/lua/mr_dupin/plugins/lsp/mason.lua
@@ -1,56 +1,58 @@
 -- https://github.com/williamboman/mason.nvim
-return{
-  "williamboman/mason.nvim",
+return {
+  'williamboman/mason.nvim',
   dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim"
+    'williamboman/mason-lspconfig.nvim',
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
   },
   config = function()
-    local mason = require("mason")
+    local mason = require 'mason'
 
-    local mason_lspconfig = require("mason-lspconfig")
+    local mason_lspconfig = require 'mason-lspconfig'
 
-    local mason_tool_installer = require("mason-tool-installer")
+    local mason_tool_installer = require 'mason-tool-installer'
 
-    mason.setup({
+    mason.setup {
       ui = {
         icons = {
-          package_installed = "󰅿 ",
-          package_pending = "󰦗 ",
-          package_uninstalled = "󰭽 ",
+          package_installed = '󰅿 ',
+          package_pending = '󰦗 ',
+          package_uninstalled = '󰭽 ',
         },
       },
-    })
+    }
 
-    mason_lspconfig.setup({
+    mason_lspconfig.setup {
       ensure_installed = {
-        "tsserver",
-        "html",
-        "htmx",
-        "cssls",
-        "tailwindcss",
-        "svelte",
-        "lua_ls",
-        "bashls",
-        "dockerls",
-        "docker_compose_language_service",
-        "gopls",
-        "markdown_oxide",
-        "rust_analyzer",
-        "sqls",
-        "solang",
-        "templ",
-        "yamlls",
+        'tsserver',
+        'html',
+        'htmx',
+        'cssls',
+        'tailwindcss',
+        'svelte',
+        'lua_ls',
+        'bashls',
+        'dockerls',
+        'docker_compose_language_service',
+        'gopls',
+        'markdown_oxide',
+        'rust_analyzer',
+        'sqls',
+        'solang',
+        'templ',
+        'yamlls',
       },
-    })
+    }
 
-    mason_tool_installer.setup({
-      -- list of formatters
+    mason_tool_installer.setup {
+      -- list of formatters & linters
       ensure_installed = {
-        "prettier",
-        "stylua"
+        -- formatters
+        'prettier',
+        'stylua',
+        -- linters
+        'eslint_d',
       },
-    })
-
-  end
+    }
+  end,
 }
