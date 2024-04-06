dd52a758f056e07b632f6ac470e26562230e677f
Phanor Coll <phanorcoll@gmail.com>
2024-04-06 19:09:49 -0400

    chore: Update icons

--- .config/nvim/lua/mr_dupin/plugins/nvim-tree.lua
+++ .config/nvim/lua/mr_dupin/plugins/nvim-tree.lua
@@ -1,18 +1,16 @@
 -- https://github.com/nvim-tree/nvim-tree.lua
--- TODO: Improve configuration
--- TODO: Make bg transparent
 -- :h nvim-tree
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
+  'nvim-tree/nvim-tree.lua',
+  version = '*',
   lazy = false,
   dependencies = {
-    "nvim-tree/nvim-web-devicons",
+    'nvim-tree/nvim-web-devicons',
   },
   config = function()
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPugin = 1
-    require("nvim-tree").setup({
+    require('nvim-tree').setup {
       view = {
         width = 35,
         relativenumber = true,
@@ -24,8 +22,8 @@ return {
         icons = {
           glyphs = {
             folder = {
-              arrow_closed = " ",
-              arrow_open = " "
+              arrow_closed = '󰁕 ',
+              arrow_open = ' ',
             },
           },
         },
@@ -41,13 +39,13 @@ return {
       git = {
         ignore = true,
       },
-    })
+    }
 
     -- keymapping
     local keymap = vim.keymap
-    keymap.set("n","<leader>ee","<cmd>NvimTreeToggle<CR>",{ desc = "Toggle file explorer"})
-    keymap.set("n","<leader>ef","<cmd>NvimTreeFindFileToggle<CR>",{ desc = "Open file explorer on current file"})
-    keymap.set("n","<leader>ec","<cmd>NvimTreeCollapse<CR>",{ desc = "Collapse folders"})
-    keymap.set("n","<leader>er","<cmd>NvimTreeRefresh<CR>",{ desc = "Refresh file explorer"})
+    keymap.set('n', '<leader>ee', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle file explorer' })
+    keymap.set('n', '<leader>ef', '<cmd>NvimTreeFindFileToggle<CR>', { desc = 'Open file explorer on current file' })
+    keymap.set('n', '<leader>ec', '<cmd>NvimTreeCollapse<CR>', { desc = 'Collapse folders' })
+    keymap.set('n', '<leader>er', '<cmd>NvimTreeRefresh<CR>', { desc = 'Refresh file explorer' })
   end,
 }
