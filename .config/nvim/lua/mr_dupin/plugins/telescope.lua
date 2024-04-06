c84f713d41ddc31c7e08806ba6885047e3fb72ea
Phanor Coll <phanorcoll@gmail.com>
2024-04-06 07:32:11 -0400

    chore: Add todo-commets package with Telescope integration

--- .config/nvim/lua/mr_dupin/plugins/telescope.lua
+++ .config/nvim/lua/mr_dupin/plugins/telescope.lua
@@ -8,6 +8,7 @@ return{
     "nvim-lua/plenary.nvim",
     {"nvim-telescope/telescope-fzf-native.nvim", build = "make"},
     "nvim-tree/nvim-web-devicons",
+    "folke/todo-comments.nvim",
   },
   config = function()
     local telescope = require("telescope")
@@ -32,5 +33,6 @@ return{
     keymap.set("n","<leader>fr","<cmd>Telescope oldfiles<CR>",{ desc = "Find recent files" })
     keymap.set("n","<leader>fs","<cmd>Telescope live_grep<CR>",{ desc = "Find string in project" })
     keymap.set("n","<leader>fc","<cmd>Telescope grep_string<CR>",{ desc = "Find string under cursor" })
+    keymap.set("n","<leader>ft","<cmd>TodoTelescope<CR>",{ desc = "Find TODOs" })
   end
 }
