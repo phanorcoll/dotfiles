65ca34528d358441efe5ff0934ad24b4f8020bc0
Phanor Coll <phanorcoll@gmail.com>
2024-04-05 20:16:29 -0400

    chore: Add lualine.nvim with initial config

---
+++ .config/nvim/lua/mr_dupin/plugins/lualine.lua
@@ -0,0 +1,12 @@
+-- https://github.com/nvim-lualine/lualine.nvim
+return{
+  "nvim-lualine/lualine.nvim",
+  dependencies = {"nvim-tree/nvim-web-devicons"},
+  config = function()
+    local lualine = require("lualine")
+
+    lualine.setup({
+      theme = "ayu_dark",
+    })
+  end
+}
