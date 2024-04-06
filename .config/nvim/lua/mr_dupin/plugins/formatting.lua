c75cd88b9f1f660fa5d532f41d97947831f0dbef
Phanor Coll <phanorcoll@gmail.com>
2024-04-06 10:28:19 -0400

    chore: Add conform.nvim for formatting, setup initial config

---
+++ .config/nvim/lua/mr_dupin/plugins/formatting.lua
@@ -0,0 +1,36 @@
+-- https://github.com/stevearc/conform.nvim
+return {
+  'stevearc/conform.nvim',
+  event = { 'BufReadPre', 'BufNewFile' },
+  config = function()
+    local conform = require 'conform'
+    conform.setup {
+      formatters_by_ft = {
+        javascript = { 'prettier' },
+        typescript = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        typescriptreact = { 'prettier' },
+        svelte = { 'prettier' },
+        css = { 'prettier' },
+        html = { 'prettier' },
+        json = { 'prettier' },
+        yaml = { 'prettier' },
+        markdown = { 'prettier' },
+        lua = { 'stylua' },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      },
+    }
+    local keymap = vim.keymap
+    keymap.set({ 'n', 'v' }, '<leader>mp', function()
+      conform.format {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      }
+    end, { desc = 'Format file or range' })
+  end,
+}
--- .config/nvim/lua/mr_dupin/plugins/lsp/mason.lua
+++ .config/nvim/lua/mr_dupin/plugins/lsp/mason.lua
@@ -3,11 +3,14 @@ return{
   "williamboman/mason.nvim",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim"
   },
   config = function()
     local mason = require("mason")
 
     local mason_lspconfig = require("mason-lspconfig")
+
+    local mason_tool_installer = require("mason-tool-installer")
 
     mason.setup({
       ui = {
@@ -38,6 +41,14 @@ return{
         "solang",
         "templ",
         "yamlls",
+      },
+    })
+
+    mason_tool_installer.setup({
+      -- list of formatters
+      ensure_installed = {
+        "prettier",
+        "stylua"
       },
     })
 
