794f09825e77e9bc1d1352d816c5e63e9685e43e
Phanor Coll <phanorcoll@gmail.com>
2024-04-07 09:13:33 -0400

    chore: Change key to [D] for diagnostics

--- .config/nvim/lua/mr_dupin/plugins/trouble.lua
+++ .config/nvim/lua/mr_dupin/plugins/trouble.lua
@@ -1,13 +1,13 @@
 -- https://github.com/folke/trouble.nvim
-return{
-  "folke/trouble.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
+return {
+  'folke/trouble.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons', 'folke/todo-comments.nvim' },
   keys = {
-    {"<leader>xx", "<cmd>TroubleToggle<CR>", desc = "Open/close trouble list"},
-    {"<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Open workspace trouble"},
-    {"<leader>xd", "<cmd>TroubleToggle document_diagnostics<CR>", desc = "Open document trouble list"},
-    {"<leader>xq", "<cmd>TroubleToggle quickfix<CR>", desc = "Open trouble quickfix list"},
-    {"<leader>xl", "<cmd>TroubleToggle loclist<CR>", desc = "Open trouble location list"},
-    {"<leader>xt", "<cmd>TodoTrouble<CR>", desc = "Open TODOs in trouble"},
+    { '<leader>dx', '<cmd>TroubleToggle<CR>', desc = 'Open/close trouble list' },
+    { '<leader>dw', '<cmd>TroubleToggle workspace_diagnostics<CR>', desc = 'Open workspace trouble' },
+    { '<leader>dd', '<cmd>TroubleToggle document_diagnostics<CR>', desc = 'Open document trouble list' },
+    { '<leader>dq', '<cmd>TroubleToggle quickfix<CR>', desc = 'Open trouble quickfix list' },
+    { '<leader>dl', '<cmd>TroubleToggle loclist<CR>', desc = 'Open trouble location list' },
+    { '<leader>t', '<cmd>TodoTrouble<CR>', desc = 'Open TODOs in trouble' },
   },
 }
