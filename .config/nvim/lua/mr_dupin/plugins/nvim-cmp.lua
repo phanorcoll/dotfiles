20a91d433638b8a9c781896b8d05991678f70177
Phanor Coll <phanorcoll@gmail.com>
2024-04-08 21:07:41 -0400

    chore: Improve nvim-cmp

--- .config/nvim/lua/mr_dupin/plugins/nvim-cmp.lua
+++ .config/nvim/lua/mr_dupin/plugins/nvim-cmp.lua
@@ -1,10 +1,13 @@
 -- https://github.com/hrsh7th/nvim-cmp
+-- https://github.com/hrsh7th/nvim-cmp/wiki
 return {
   'hrsh7th/nvim-cmp',
   event = 'InsertEnter',
   dependencies = {
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-path',
+    'hrsh7th/cmp-nvim-lua',
+    'hrsh7th/cmp-nvim-lsp',
     {
       'L3MON4D3/LuaSnip',
       version = 'v2.*',
@@ -12,7 +15,7 @@ return {
     },
     'saadparwaiz1/cmp_luasnip', -- for autocompletion
     'rafamadriz/friendly-snippets', -- snippers
-    'onsails/lspkind.nvim', -- pictograms
+    -- 'onsails/lspkind.nvim', -- pictogram
     {
       'zbirenbaum/copilot-cmp',
       config = function()
@@ -21,10 +24,39 @@ return {
     },
   },
   config = function()
+    -- local lspkind = require 'lspkind'
+    -- lspkind.init()
     local cmp = require 'cmp'
     local luasnip = require 'luasnip'
-    local lspkind = require 'lspkind'
     require('luasnip.loaders.from_vscode').lazy_load()
+    local kind_icons = {
+      Text = '',
+      Method = '󰆧',
+      Function = '󰊕',
+      Constructor = '',
+      Field = '󰇽',
+      Variable = '󰂡',
+      Class = '󰠱',
+      Interface = '',
+      Module = '',
+      Property = '󰜢',
+      Unit = '',
+      Value = '󰎠',
+      Enum = '',
+      Keyword = '󰌋',
+      Snippet = '',
+      Color = '󰏘',
+      File = '󰈙',
+      Reference = '',
+      Folder = '󰉋',
+      EnumMember = '',
+      Constant = '󰏿',
+      Struct = '',
+      Event = '',
+      Operator = '󰆕',
+      TypeParameter = '󰅲',
+      Copilot = '',
+    }
 
     cmp.setup {
       completion = {
@@ -42,23 +74,38 @@ return {
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm { select = false },
+        ['<CR>'] = cmp.mapping.confirm { select = true },
       },
       sources = cmp.config.sources {
         { name = 'copilot', group_index = 2 },
+        { name = 'nvim_lua', group_index = 2 },
         { name = 'nvim_lsp', group_index = 2 },
-        { name = 'luasnip', group_index = 2 },
-        { name = 'buffer', group_index = 2 },
         { name = 'path', group_index = 2 },
+        { name = 'luasnip', group_index = 2 },
+        { name = 'buffer', group_index = 2, keyword_length = 5 },
         { name = 'vim-dadbod-completion', group_index = 2 },
       },
       formatting = {
-        fields = { cmp.ItemField.Abbr, cmp.ItemField.Kind },
         expandable_indicator = false,
-        format = lspkind.cmp_format {
-          maxwidth = 50,
-          ellipsis_char = '...',
-        },
+        fields = { 'abbr', 'kind', 'menu' },
+        format = function(entry, vim_item)
+          -- Kind icons
+          vim_item.kind = string.format('%s  %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatenates the icons with the name of the item kind
+          -- Source
+          vim_item.menu = ({
+            nvim_lsp = '[LSP]',
+            nvim_lua = '[api]',
+            luasnip = '[snip]',
+            buffer = '[buffer]',
+            path = '[path]',
+            copilot = '[ai]',
+            ['vim-dadbod-completion'] = '[dadbod]',
+          })[entry.source.name]
+          return vim_item
+        end,
+      },
+      experimental = {
+        ghost_text = true,
       },
     }
   end,
