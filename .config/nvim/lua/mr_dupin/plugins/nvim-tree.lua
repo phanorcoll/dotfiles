fa771aca38439acf3245dc35692f70ed2b253dc1
Phanor Coll <phanorcoll@gmail.com>
2024-04-18 17:55:55 -0400

    chore(nvim-tree): Find and Focus Directory (with Telescope)

--- .config/nvim/lua/mr_dupin/plugins/nvim-tree.lua
+++ .config/nvim/lua/mr_dupin/plugins/nvim-tree.lua
@@ -14,6 +14,7 @@ return {
     api.events.subscribe(api.events.Event.FileCreated, function(file)
       vim.cmd('edit ' .. file.fname)
     end)
+
     require('nvim-tree').setup {
       view = {
         width = 40,
@@ -47,11 +48,33 @@ return {
       },
     }
 
+    local function find_directory_and_focus()
+      local actions = require 'telescope.actions'
+      local action_state = require 'telescope.actions.state'
+
+      local function open_nvim_tree(prompt_bufnr, _)
+        actions.select_default:replace(function()
+          local api = require 'nvim-tree.api'
+
+          actions.close(prompt_bufnr)
+          local selection = action_state.get_selected_entry()
+          api.tree.open()
+          api.tree.find_file(selection.cwd .. '/' .. selection.value)
+        end)
+        return true
+      end
+
+      require('telescope.builtin').find_files {
+        find_command = { 'fd', '--type', 'directory', '--hidden', '--exclude', '.git/*' },
+        attach_mappings = open_nvim_tree,
+      }
+    end
     -- keymapping
     local keymap = vim.keymap
     keymap.set('n', '<leader>ee', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle file explorer' })
     keymap.set('n', '<leader>ef', '<cmd>NvimTreeFindFileToggle<CR>', { desc = 'Open file explorer on current file' })
     keymap.set('n', '<leader>ec', '<cmd>NvimTreeCollapse<CR>', { desc = 'Collapse folders' })
     keymap.set('n', '<leader>er', '<cmd>NvimTreeRefresh<CR>', { desc = 'Refresh file explorer' })
+    keymap.set('n', 'fd', find_directory_and_focus)
   end,
 }
