c19a0b20eca3c05f2c6bf3c01c4a4a2bedc12678
Phanor Coll <phanorcoll@gmail.com>
2024-11-30 09:04:20 -0400

    chore(options): Add options and two functions

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,8 +1,7 @@
 local opt = vim.opt
-opt.nu = true 			-- enable line numbers
-opt.relativenumber = true 		-- relative line numbers
---clipboard
-opt.clipboard:append 'unnamedplus'
+opt.nu = true -- enable line numbers
+opt.relativenumber = true -- relative line numbers
+opt.clipboard:append 'unnamedplus' --clipboard
 
 -- tabs & indentation
 opt.tabstop = 2
@@ -20,3 +19,34 @@ opt.cursorline = true
 -- window splitting
 opt.splitright = true
 opt.splitbelow = true
+
+opt.list = true -- show tab characters and trailing whitespace
+
+opt.swapfile = false -- do not use a swap file for the buffer
+opt.backup = false -- do not keep a backup file
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir" -- set directory where undo files are stored
+opt.undofile = true -- save undo history to a file
+
+opt.signcolumn = "yes" -- always show the sign column, to avoid text shifting when signs are displayed
+opt.updatetime = 50 -- Time in milliseconds to wait before triggering the plugin events after a change
+
+------- functions
+
+local CleanOnSave = vim.api.nvim_create_augroup('CleanOnSave', {})
+vim.api.nvim_create_autocmd({"BufWritePre"}, {
+  group = CleanOnSave,
+  pattern = "*",
+  command = [[%s/\s\+$//e]],
+}) -- remove trailing whitespace from all lines before saving a file)
+
+local HighlightYank = vim.api.nvim_create_augroup('HighlightYank', {})
+vim.api.nvim_create_autocmd('TextYankPost', {
+    group = HighlightYank,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 400,
+        })
+    end,
+}) -- highlight yanked text using the 'IncSearch' highlight group for 40ms
