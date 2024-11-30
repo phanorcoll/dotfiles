650b5ed1f8fefebcd58212a9edbae1abcd4469f0
Phanor Coll <phanorcoll@gmail.com>
2024-11-30 14:42:24 -0400

    chore(plugins): Change the keymap to view buffers in telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -5,11 +5,11 @@ return {
   tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
   keys = {
-    { '<leader>ff', builtin.find_files, desc = 'Telescope find files' },
-    { '<leader>fg', builtin.live_grep,  desc = 'Telescope live grep' },
-    { '<leader>fb', builtin.buffers,    desc = 'Telescope buffers' },
-    { '<leader>fh', builtin.help_tags,  desc = 'Telescope help tags' },
-    { '<leader>fo', builtin.oldfiles,   desc = 'Telescope help tags' },
-    { '<leader>gf', builtin.git_files,  desc = 'Telescope help tags' },
+    { '<leader>ff',       builtin.find_files, desc = 'Telescope find files' },
+    { '<leader>fg',       builtin.live_grep,  desc = 'Telescope live grep' },
+    { '<leader><leader>', builtin.buffers,    desc = 'Telescope buffers' },
+    { '<leader>fh',       builtin.help_tags,  desc = 'Telescope help tags' },
+    { '<leader>fo',       builtin.oldfiles,   desc = 'Telescope help tags' },
+    { '<leader>gf',       builtin.git_files,  desc = 'Telescope help tags' },
   },
 }
