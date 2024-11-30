0df3f6b08a8e307aa44a2c7844e1105c8f20c8bb
Phanor Coll <phanorcoll@gmail.com>
2024-11-29 21:30:24 -0400

    chore(plugins): Add Markdown Preview

---
+++ .config/nvim/lua/plugins/markdown_previewer.lua
@@ -0,0 +1,10 @@
+vim.keymap.set('n', '<leader>mp', "<cmd>MarkdownPreviewToggle<cr>", { desc = 'Toggles Markdown Preview' })
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && yarn install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  ft = { "markdown" },
+}
