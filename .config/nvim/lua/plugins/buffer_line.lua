9bd970a9b769faf050d044e4cd3c88f95a52d9b6
Phanor Coll <phanorcoll@gmail.com>
2024-11-30 11:40:41 -0400

    chore(plugins): Disable Buffer lines lazy loading
    Move the keymaps to the beginnig to load the plugin when neovim loads.

--- .config/nvim/lua/plugins/buffer_line.lua
+++ .config/nvim/lua/plugins/buffer_line.lua
@@ -1,13 +1,11 @@
-return{
+vim.keymap.set("n", "<leader>n", "<cmd>bn<cr>", { desc = 'Move to next buffer' })
+vim.keymap.set("n", "<leader>p", "<cmd>bp<cr>", { desc = 'Move to previous buffer' })
+vim.keymap.set("n", "<leader>x", "<cmd>bd<cr>", { desc = 'Close buffer' })
+return {
   'akinsho/bufferline.nvim',
   version = "*",
   dependencies = 'nvim-tree/nvim-web-devicons',
-  keys ={
-    {"<leader>n","<cmd>bn<cr>",  desc = 'Move to next buffer' },
-    {"<leader>p","<cmd>bp<cr>",  desc = 'Move to previous buffer' },
-    {"<leader>x","<cmd>bd<cr>",  desc = 'Close buffer' },
-  },
   config = function()
-    require("bufferline").setup{}
+    require("bufferline").setup {}
   end
 }
