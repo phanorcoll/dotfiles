60f4605e6dcaca1856cae29200623967c13f8a36
Phanor Coll <phanorcoll@gmail.com>
2024-11-30 08:33:38 -0400

    chore(plugins): Move keymappings to the keys object
    Key mappings will load the plugin the first time they get executed.

--- .config/nvim/lua/plugins/nvim_tree.lua
+++ .config/nvim/lua/plugins/nvim_tree.lua
@@ -1,11 +1,13 @@
-vim.keymap.set('n', '<leader>ee', "<cmd>NvimTreeFindFileToggle<cr>", { desc = 'Toggle Nvim Tree' })
-vim.keymap.set('n', '<leader>er', '<cmd>NvimTreeRefresh<CR>', { desc = 'Refresh file explorer' })
 return {
   "nvim-tree/nvim-tree.lua",
   version = "*",
   lazy = false,
   dependencies = {
     "nvim-tree/nvim-web-devicons",
+  },
+  keys={
+    {'<leader>ee', "<cmd>NvimTreeFindFileToggle<cr>", desc = 'Toggle Nvim Tree' },
+    {'<leader>er', '<cmd>NvimTreeRefresh<CR>',  desc = 'Refresh file explorer' },
   },
   config = function()
     require("nvim-tree").setup {}
