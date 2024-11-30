f90be20738c8345c29c4882bd1982d73d4307cd8
Phanor Coll <phanorcoll@gmail.com>
2024-11-29 20:52:38 -0400

    chore(plugins): Add Catppuccin theme

---
+++ .config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,11 @@
+return { 
+	"catppuccin/nvim", name = "catppuccin", priority = 1000,
+	config = function()
+    require("catppuccin").setup({
+      flavour = "mocha",
+      transparent_background = false,
+      term_colors = true
+    })
+		vim.cmd.colorscheme "catppuccin-mocha"
+	end,
+}
