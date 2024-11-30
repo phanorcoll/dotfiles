208580e3a57ea0866c5854b39b5b918acd851794
Phanor Coll <phanorcoll@gmail.com>
2024-11-30 09:55:49 -0400

    chore(plugins): Telescope apply formatting and fix keys

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,15 +1,15 @@
 local builtin = require('telescope.builtin')
 
 return {
-    'nvim-telescope/telescope.nvim', 
-    tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' },
-      keys={
-        {'<leader>ff', builtin.find_files,  desc = 'Telescope find files' },
-        {'<leader>fg', builtin.live_grep,  desc = 'Telescope live grep' },
-        {'n', '<leader>fb', builtin.buffers,  desc = 'Telescope buffers' },
-        {'n', '<leader>fh', builtin.help_tags,  desc = 'Telescope help tags' },
-        {'n', '<leader>fo', builtin.oldfiles,  desc = 'Telescope help tags' },
-        {'n', '<leader>gf', builtin.git_files,  desc = 'Telescope help tags' },
-      },
-    }
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  keys = {
+    { '<leader>ff', builtin.find_files, desc = 'Telescope find files' },
+    { '<leader>fg', builtin.live_grep,  desc = 'Telescope live grep' },
+    { '<leader>fb', builtin.buffers,    desc = 'Telescope buffers' },
+    { '<leader>fh', builtin.help_tags,  desc = 'Telescope help tags' },
+    { '<leader>fo', builtin.oldfiles,   desc = 'Telescope help tags' },
+    { '<leader>gf', builtin.git_files,  desc = 'Telescope help tags' },
+  },
+}
