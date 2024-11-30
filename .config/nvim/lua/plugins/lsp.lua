63b9bcece0f6482e4ec41af9285c52c0f490adbf
Phanor Coll <phanorcoll@gmail.com>
2024-11-30 18:43:58 -0400

    chore(plugins): Add new LSPs, formatters and linters to Mason

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -142,6 +142,7 @@ return {
       { 'hrsh7th/cmp-nvim-lsp' },
       { 'williamboman/mason.nvim' },
       { 'williamboman/mason-lspconfig.nvim' },
+      { 'WhoIsSethDaniel/mason-tool-installer.nvim' },
     },
     init = function()
       -- Reserve a space in the gutter
@@ -241,7 +242,21 @@ return {
       })
 
       require('mason-lspconfig').setup({
-        ensure_installed = { "lua_ls", "denols", "html", "htmx", "templ" },
+        ensure_installed = {
+          "lua_ls",
+          "denols",
+          "html",
+          "htmx",
+          "templ",
+          "gopls",
+          "cssls",
+          "tailwindcss",
+          "bashls",
+          "dockerls",
+          "docker_compose_language_service",
+          "markdown_oxide",
+          "yamlls"
+        },
         handlers = {
           -- this first function is the "default handler"
           -- it applies to every language server without a "custom handler"
@@ -249,6 +264,48 @@ return {
             require('lspconfig')[server_name].setup({})
           end,
         }
+      })
+
+      -- list of formatters & linters
+      require('mason-tool-installer').setup({
+        ensure_installed = {
+          -- formatters
+          'prettier',
+          'stylua',
+          'gofumpt',
+          'goimports',
+          'golines',
+          -- linters
+          'revive',
+          'markdownlint',
+          'jsonlint',
+          'eslint_d',
+        },
+        -- if set to true this will check each tool for updates. If updates
+        -- are available the tool will be updated. This setting does not
+        -- affect :MasonToolsUpdate or :MasonToolsInstall.
+        -- Default: false
+        auto_update = false,
+
+        -- automatically install / update on startup. If set to false nothing
+        -- will happen on startup. You can use :MasonToolsInstall or
+        -- :MasonToolsUpdate to install tools and check for updates.
+        -- Default: true
+        run_on_start = true,
+
+        -- set a delay (in ms) before the installation starts. This is only
+        -- effective if run_on_start is set to true.
+        -- e.g.: 5000 = 5 second delay, 10000 = 10 second delay, etc...
+        -- Default: 0
+        start_delay = 2000, -- 3 second delay
+
+        -- Only attempt to install if 'debounce_hours' number of hours has
+        -- elapsed since the last time Neovim was started. This stores a
+        -- timestamp in a file named stdpath('data')/mason-tool-installer-debounce.
+        -- This is only relevant when you are using 'run_on_start'. It has no
+        -- effect when running manually via ':MasonToolsInstall' etc....
+        -- Default: nil
+        debounce_hours = 5, -- at least 5 hours between attempts to install/update
       })
     end
   }
