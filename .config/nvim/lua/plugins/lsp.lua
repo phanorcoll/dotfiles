74bfce4e02570301842562332f5d298e74daf8d0
Phanor Coll <phanorcoll@gmail.com>
2024-11-30 10:34:23 -0400

    chore(plugins): Denols & format on save
    - Add denosl LSP for javascript,typescript files
    - Setup format on save

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,3 +1,6 @@
+vim.g.markdown_fenced_languages = {
+  "ts=typescript"
+}
 return {
   {
     'williamboman/mason.nvim',
@@ -56,11 +59,33 @@ return {
         require('cmp_nvim_lsp').default_capabilities()
       )
 
+      local buffer_autoformat = function(bufnr)
+        local group = 'lsp_autoformat'
+        vim.api.nvim_create_augroup(group, { clear = false })
+        vim.api.nvim_clear_autocmds({ group = group, buffer = bufnr })
+
+        vim.api.nvim_create_autocmd('BufWritePre', {
+          buffer = bufnr,
+          group = group,
+          desc = 'LSP format on save',
+          callback = function()
+            -- note: do not enable async formatting
+            vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+          end,
+        })
+      end
+
       -- LspAttach is where you enable features that only work
       -- if there is a language server active in the file
       vim.api.nvim_create_autocmd('LspAttach', {
         desc = 'LSP actions',
         callback = function(event)
+          local id = vim.tbl_get(event, 'data', 'client_id')
+          local client = id and vim.lsp.get_client_by_id(id)
+          if client == nil then
+            return
+          end
+
           local opts = { buffer = event.buf }
 
           vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
@@ -73,11 +98,16 @@ return {
           vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
           vim.keymap.set({ 'n', 'x' }, '<leader>fd', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
           vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+
+          -- make sure there is at least one client with formatting capabilities
+          if client.supports_method('textDocument/formatting') then
+            buffer_autoformat(event.buf)
+          end
         end,
       })
 
       require('mason-lspconfig').setup({
-        ensure_installed = { "lua_ls" },
+        ensure_installed = { "lua_ls", "denols" },
         handlers = {
           -- this first function is the "default handler"
           -- it applies to every language server without a "custom handler"
