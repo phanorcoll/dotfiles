7cb7f6fe407be77debaa650b7fb4759e8bd9411f
Phanor Coll <phanorcoll@gmail.com>
2024-11-30 10:05:41 -0400

    chore(plugins): Apply LSP formatting

--- .config/nvim/lua/plugins/buffer_line.lua
+++ .config/nvim/lua/plugins/buffer_line.lua
@@ -1,6 +1,6 @@
 return{
-  'akinsho/bufferline.nvim', 
-  version = "*", 
+  'akinsho/bufferline.nvim',
+  version = "*",
   dependencies = 'nvim-tree/nvim-web-devicons',
   keys ={
     {"<leader>n","<cmd>bn<cr>",  desc = 'Move to next buffer' },
