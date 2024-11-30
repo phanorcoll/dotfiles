4cb9621129d7974e0dedc1dbd2e9b7aeee1b4646
Phanor Coll <phanorcoll@gmail.com>
2024-11-30 13:07:32 -0400

    chore(plugins): Fix undefined type for auto_session

--- .config/nvim/lua/plugins/auto_session.lua
+++ .config/nvim/lua/plugins/auto_session.lua
@@ -4,7 +4,6 @@ return {
 
   ---enables autocomplete for opts
   ---@module "auto-session"
-  ---@type AutoSession.Config
   opts = {
     suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
     log_level = 'error',
