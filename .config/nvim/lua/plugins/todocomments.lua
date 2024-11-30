dd716659e0b219490b5e93af56265ab7af5d73a6
Phanor Coll <phanorcoll@gmail.com>
2024-11-30 13:03:03 -0400

    chore(plugins): Add todocomments

---
+++ .config/nvim/lua/plugins/todocomments.lua
@@ -0,0 +1,11 @@
+vim.keymap.set("n", "<leader>td", "<cmd>TodoTelescope<cr>", { desc = "List  TODO comments" })
+return
+{
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  }
+}
