dcb848502ded288bdaf15e8c453940abe6e87d19
Phanor Coll <phanorcoll@gmail.com>
2024-12-11 07:15:21 -0400

    chore(plugins): Add fzf integration to telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -5,7 +5,23 @@ local builtin = require('telescope.builtin')
 return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  },
+  config = function()
+    require('telescope').setup {
+      pickers = {
+        find_files = {
+          theme = 'ivy',
+        }
+      },
+      extensions = {
+        fzf = {}
+      }
+    }
+    require('telescope').load_extension('fzf')
+  end,
   keys = {
     { '<leader>ff',       builtin.find_files, desc = 'Telescope find files' },
     { '<leader>fg',       builtin.live_grep,  desc = 'Telescope live grep' },
