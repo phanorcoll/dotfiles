09934bdef5fee446bfae0ab9976af5f24e6f0573
Phanor Coll <phanorcoll@gmail.com>
2024-12-18 07:11:00 -0400

    chore(plugins): Add new file explorer neogit

---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,58 @@
+-- url: https://github.com/stevearc/oil.nvim
+-- desc: File explorer that lets you edit your filesystem like a normal Neovim buffer.
+return {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  config = function()
+    local detail = false
+    function _G.get_oil_winbar()
+      local dir = require("oil").get_current_dir()
+      if dir then
+        return vim.fn.fnamemodify(dir, ":~")
+      else
+        -- If there is no current directory (e.g. over ssh), just show the buffer name
+        return vim.api.nvim_buf_get_name(0)
+      end
+    end
+
+    require('oil').setup({
+      win_options = {
+        winbar = "%!v:lua.get_oil_winbar()",
+      },
+      keymaps = {
+        ["gd"] = {
+          desc = "Toggle file detail view",
+          callback = function()
+            detail = not detail
+            if detail then
+              require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+            else
+              require("oil").set_columns({ "icon" })
+            end
+          end,
+        },
+        ["g?"] = { "actions.show_help", mode = "n" },
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+        ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+        ["<C-t>"] = { "actions.select", opts = { tab = true } },
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = { "actions.close", mode = "n" },
+        ["<C-l>"] = "actions.refresh",
+        ["-"] = { "actions.parent", mode = "n" },
+        ["_"] = { "actions.open_cwd", mode = "n" },
+        ["`"] = { "actions.cd", mode = "n" },
+        ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+        ["gs"] = { "actions.change_sort", mode = "n" },
+        ["gx"] = "actions.open_external",
+        ["g."] = { "actions.toggle_hidden", mode = "n" },
+        ["g\\"] = { "actions.toggle_trash", mode = "n" },
+      },
+    })
+    local keymap = vim.keymap
+    keymap.set("n", "<leader>o", "<cmd>Oil<CR>", { desc = "Toggle oil" })
+  end,
+}
