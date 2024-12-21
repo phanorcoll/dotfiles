6d45c22b88ecc7e6abe5d8324136e49119cf32f4
Phanor Coll <phanorcoll@gmail.com>
2024-12-21 09:57:33 -0400

    chore(plugins): Fix Codesnap key mappings, add paddings

--- .config/nvim/lua/plugins/codesnap.lua
+++ .config/nvim/lua/plugins/codesnap.lua
@@ -6,12 +6,15 @@ return {
   keys = {
     { '<leader>cc', '<cmd>CodeSnap<cr>', mode = 'x', desc = 'Save selected code snapshot into clipboard' },
     { '<leader>cs', '<cmd>CodeSnapSave<cr>', mode = 'x', desc = 'Save selected code snapshot in ~/Pictures' },
-    { '<leader>ch', '<cmd>CodeSnapSaveHighlight<cr>', mode = 'x', desc = 'Save selected code snapshot in ~/Pictures' },
-    { '<leader>ca', '<cmd>CodeSnapASCIIcr>', mode = 'x', desc = 'Save selected code in ASCII formar' },
+    { '<leader>ca', '<cmd>CodeSnapASCII<cr>', mode = 'x', desc = 'Save selected code in ASCII formar' },
+    { '<leader>ch', '<cmd>CodeSnapSaveHighlight<cr>', mode = 'n', desc = 'Highlight code' },
   },
   opts = {
     code_font_family = 'CaskaydiaCove Nerd Font',
     bg_theme = 'grape',
     watermark = 'Phanor Coll',
+    bg_x_padding = 20,
+    bg_y_padding = 90,
+    bg_padding = nil,
   },
 }
