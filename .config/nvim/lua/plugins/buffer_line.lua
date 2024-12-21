935003372323223f85b85570ca696c145e757c91
Phanor Coll <phanorcoll@gmail.com>
2024-12-21 10:11:10 -0400

    chore(plugins): Change mappings for buffer_line

--- .config/nvim/lua/plugins/buffer_line.lua
+++ .config/nvim/lua/plugins/buffer_line.lua
@@ -1,7 +1,7 @@
 -- url: https://githob.com/akinsho/bufferline.nvim
 -- desc: Bufferline is a snazzy buffer line for Neovim
-vim.keymap.set('n', '<leader>n', '<cmd>bn<cr>', { desc = 'Move to next buffer' })
-vim.keymap.set('n', '<leader>p', '<cmd>bp<cr>', { desc = 'Move to previous buffer' })
+vim.keymap.set('n', '<leader>nb', '<cmd>bn<cr>', { desc = 'Move to next buffer' })
+vim.keymap.set('n', '<leader>pb', '<cmd>bp<cr>', { desc = 'Move to previous buffer' })
 vim.keymap.set('n', '<leader>cb', '<cmd>bd<cr>', { desc = 'Close buffer' })
 return {
   'akinsho/bufferline.nvim',
