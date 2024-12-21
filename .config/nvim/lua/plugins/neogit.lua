11c8583dd410cf2e0ed10ebbcd69fcd04efe3df1
Phanor Coll <phanorcoll@gmail.com>
2024-12-21 10:16:59 -0400

    chore(plugins): Add neogit mappings, apply formatting

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -1,36 +1,34 @@
 -- url: https://github.com/NeogitOrg/neogit
 -- desc: Neogit is a magit like git client for neovim
 return {
-  "NeogitOrg/neogit",
+  'NeogitOrg/neogit',
   dependencies = {
-    "nvim-lua/plenary.nvim",  -- required
-    "sindrets/diffview.nvim", -- optional - Diff integration
+    'nvim-lua/plenary.nvim', -- required
+    'sindrets/diffview.nvim', -- optional - Diff integration
 
     -- Only one of these is needed.
-    "ibhagwan/fzf-lua", -- optional
+    'ibhagwan/fzf-lua', -- optional
   },
   config = function()
-    require("neogit").setup {
+    require('neogit').setup {
       disable_signs = false,
       disable_context_highlighting = false,
       disable_commit_confirmation = false,
       disable_builtin_notifications = false,
       auto_refresh = true,
-      graph_style = "unicode",
+      graph_style = 'unicode',
       commit_popup = {
-        kind = "split",
+        kind = 'split',
       },
       integrations = {
         diffview = true,
       },
     }
     local keymap = vim.keymap
-    keymap.set("n", "<leader>gs", ":Neogit kind=floating<CR>", { desc = "Open Neogit", silent = true, noremap = true })
-    keymap.set("n", "<leader>gc", ":Neogit commit<CR>",
-      { desc = "Create commit", silent = true, noremap = true })
-    keymap.set("n", "<leader>gp", ":Neogit pull<CR>",
-      { desc = "Pull changes to branch", silent = true, noremap = true })
-    keymap.set("n", "<leader>gP", ":Neogit push<CR>",
-      { desc = "Push changes to branch", silent = true, noremap = true })
-  end
+    keymap.set('n', '<leader>gs', ':Neogit kind=floating<CR>', { desc = 'Open Neogit Floating window', silent = true, noremap = true })
+    keymap.set('n', '<leader>gn', ':Neogit<CR>', { desc = 'Open Neogit' })
+    keymap.set('n', '<leader>gc', ':Neogit commit<CR>', { desc = 'Create commit', silent = true, noremap = true })
+    keymap.set('n', '<leader>gp', ':Neogit pull<CR>', { desc = 'Pull changes to branch', silent = true, noremap = true })
+    keymap.set('n', '<leader>gP', ':Neogit push<CR>', { desc = 'Push changes to branch', silent = true, noremap = true })
+  end,
 }
