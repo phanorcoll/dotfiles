98b72c1ad55f1050e45d1785325735407895d9fd
Phanor Coll <phanorcoll@gmail.com>
2025-04-13 11:24:30 -0400

    chore(vim-sleuth): add package to keep tabs,spaces

---
+++ .config/nvim-v2/lua/plugins/sleuth.lua
@@ -0,0 +1,5 @@
+-- url: https://github.com/tpope/vim-sleuth
+-- desc: automatically adjusts 'shiftwidth' and 'expandtab' heuristically based on the current file.
+return{
+  "https://github.com/tpope/vim-sleuth"
+}
