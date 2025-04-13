0e1a9e31c68a84b9053a03ff4c12f98a98bfe45c
Phanor Coll <phanorcoll@gmail.com>
2025-04-13 12:29:48 -0400

    chore(kanagawa): override default colors for markdown

--- .config/nvim-v2/lua/plugins/kanagawa.lua
+++ .config/nvim-v2/lua/plugins/kanagawa.lua
@@ -1,38 +1,46 @@
 -- url: https://github.com/rebelot/kanagawa.nvim
 -- desc: Kanagawa color scheme for Neovim
 return { 
-	"rebelot/kanagawa.nvim",
-	config=function()
-		-- Default options:
-require('kanagawa').setup({
-    compile = true,             -- enable compiling the colorscheme
-    undercurl = true,            -- enable undercurls
-    commentStyle = { italic = true },
-    functionStyle = {},
-    keywordStyle = { italic = true},
-    statementStyle = { bold = true },
-    typeStyle = {},
-    transparent = true,         -- do not set background color
-    dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
-    terminalColors = true,       -- define vim.g.terminal_color_{0,17}
-    colors = {                   -- add/modify theme and palette colors
-        palette = {},
-        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-    },
-    overrides = function(colors) -- add/modify highlights
-        return {}
-    end,
-    theme = "wave",              -- Load "wave" theme
-    background = {               -- map the value of 'background' option to a theme
-        dark = "wave",           -- try "dragon" !
-        light = "lotus"
-    },
-})
+    "rebelot/kanagawa.nvim",
+    config=function()
+        -- Default options:
+        require('kanagawa').setup({
+            compile = true,             -- enable compiling the colorscheme
+            undercurl = true,            -- enable undercurls
+            commentStyle = { italic = true },
+            functionStyle = {},
+            keywordStyle = { italic = true},
+            statementStyle = { bold = true },
+            typeStyle = {},
+            transparent = true,         -- do not set background color
+            dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+            terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+            colors = {                   -- add/modify theme and palette colors
+                palette = {},
+                theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+            },
+            overrides = function(colors) -- add/modify highlights
+                return {
+                    ["@markup.link.url.markdown_inline"] = { link = "Special" }, -- (url)
+                    ["@markup.link.label.markdown_inline"] = { link = "WarningMsg" }, -- [label]
+                    ["@markup.italic.markdown_inline"] = { link = "Exception" }, -- *italic*
+                    ["@markup.raw.markdown_inline"] = { link = "String" }, -- `code`
+                    ["@markup.list.markdown"] = { link = "Function" }, -- + list
+                    ["@markup.quote.markdown"] = { link = "Error" }, -- > blockcode
+                    ["@markup.list.checked.markdown"] = { link = "WarningMsg" } -- - [X] checked list item
+                }
+            end,
+            theme = "wave",              -- Load "wave" theme
+            background = {               -- map the value of 'background' option to a theme
+                dark = "wave",           -- try "dragon" !
+                light = "lotus"
+            },
+        })
 
--- setup must be called before loading
-vim.cmd("colorscheme kanagawa")
-end,
-build=function()
-	vim.cmd("KanagawaCompile")
-end
+        -- setup must be called before loading
+        vim.cmd("colorscheme kanagawa")
+    end,
+    build=function()
+        vim.cmd("KanagawaCompile")
+    end
 }
