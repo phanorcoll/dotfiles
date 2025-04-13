ad537bb00cd7ea1eddef3bbcebd167ebacb9f220
Phanor Coll <phanorcoll@gmail.com>
2025-04-12 20:31:25 -0400

    chore(oil.nvim): add configutation

---
+++ .config/nvim-v2/lua/plugins/oil_nvim.lua
@@ -0,0 +1,126 @@
+-- url: https://github.com/stevearc/oil.nvim
+-- desc: File explorer like a text buffer
+return {
+  "stevearc/oil.nvim",
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  keys = {
+      { "-", "<CMD>Oil<CR>", desc = "Open parent directory with oil" },
+    },
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+      config = function()
+    local detail = false
+    function _G.get_oil_winbar()
+      local dir = require("oil").get_current_dir()
+      if dir then
+        return vim.fn.fnamemodify(dir, ":~")
+      else
+        -- If there is no current directory (e.g. over ssh), just show the buffer name
+        return vim.api.nvim_buf_get_name(0)
+      end
+    end
+
+    local function parse_output(proc)
+      local result = proc:wait()
+      local ret = {}
+      if result.code == 0 then
+        for line in vim.gsplit(result.stdout, "\n", { plain = true, trimempty = true }) do
+          -- Remove trailing slash
+          line = line:gsub("/$", "")
+          ret[line] = true
+        end
+      end
+      return ret
+    end
+
+    -- build git status cache
+    local function new_git_status()
+      return setmetatable({}, {
+        __index = function(self, key)
+          local ignore_proc = vim.system(
+            { "git", "ls-files", "--ignored", "--exclude-standard", "--others", "--directory" },
+            {
+              cwd = key,
+              text = true,
+            }
+          )
+          local tracked_proc = vim.system({ "git", "ls-tree", "HEAD", "--name-only" }, {
+            cwd = key,
+            text = true,
+          })
+          local ret = {
+            ignored = parse_output(ignore_proc),
+            tracked = parse_output(tracked_proc),
+          }
+
+          rawset(self, key, ret)
+          return ret
+        end,
+      })
+    end
+    local git_status = new_git_status()
+
+    -- Clear git status cache on refresh
+    local refresh = require("oil.actions").refresh
+    local orig_refresh = refresh.callback
+    refresh.callback = function(...)
+      git_status = new_git_status()
+      orig_refresh(...)
+    end
+
+    require('oil').setup({
+      view_options = {
+        is_hidden_file = function(name, bufnr)
+          local dir = require("oil").get_current_dir(bufnr)
+          local is_dotfile = vim.startswith(name, ".") and name ~= ".."
+          -- if no local directory (e.g. for ssh connections), just hide dotfiles
+          if not dir then
+            return is_dotfile
+          end
+          -- dotfiles are considered hidden unless tracked
+          if is_dotfile then
+            return not git_status[dir].tracked[name]
+          else
+            -- Check if file is gitignored
+            return git_status[dir].ignored[name]
+          end
+        end,
+      },
+      win_options = {
+        winbar = "%!v:lua.get_oil_winbar()",
+      },
+      keymaps = {
+        ["gd"] = {
+          desc = "Toggle file detail view",
+          callback = function()
+            detail = not detail
+            if detail then
+              require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+            else
+              require("oil").set_columns({ "icon" })
+            end
+          end,
+        },
+        ["g?"] = { "actions.show_help", mode = "n" },
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+        ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+        ["<C-t>"] = { "actions.select", opts = { tab = true } },
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = { "actions.close", mode = "n" },
+        ["<C-l>"] = "actions.refresh",
+        ["-"] = { "actions.parent", mode = "n" },
+        ["_"] = { "actions.open_cwd", mode = "n" },
+        ["`"] = { "actions.cd", mode = "n" },
+        ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+        ["gs"] = { "actions.change_sort", mode = "n" },
+        ["gx"] = "actions.open_external",
+        ["g."] = { "actions.toggle_hidden", mode = "n" },
+        ["g\\"] = { "actions.toggle_trash", mode = "n" },
+      },
+    })
+    local keymap = vim.keymap
+    keymap.set("n", "<leader>o", "<cmd>Oil<CR>", { desc = "Toggle oil" })
+  end,
+    lazy=false
+}
