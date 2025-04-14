542844ba5451ad06588188d9bb61876967e4606d
Phanor Coll <phanorcoll@gmail.com>
2025-04-14 17:40:57 -0400

    chore(showkeys): change position and number of keys

--- .config/nvim-v2/lua/plugins/showkeys.lua
+++ .config/nvim-v2/lua/plugins/showkeys.lua
@@ -1,13 +1,14 @@
 -- url: https://github.com/nvzone/showkeys
 -- desc: Eye-candy keys screencaster for Neovim
-return { 
-    "nvzone/showkeys", 
-    lazy = false,    -- Load on startup instead of waiting for a command
-    config = function()
-      require("showkeys").setup({
-        maxkeys=5
-      })
+return {
+	"nvzone/showkeys",
+	lazy = false, -- Load on startup instead of waiting for a command
+	config = function()
+		require("showkeys").setup({
+			maxkeys = 3,
+			position = "top-right",
+		})
 
-      vim.cmd("ShowkeysToggle")
-    end
+		vim.cmd("ShowkeysToggle")
+	end,
 }
