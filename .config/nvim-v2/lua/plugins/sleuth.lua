cbe678faf21796fdd8a9e00985477abaa6c79f19
Phanor Coll <phanorcoll@gmail.com>
2025-04-14 10:55:02 -0400

    chore(conform): add initial formatters and LSPs

--- .config/nvim-v2/lua/plugins/sleuth.lua
+++ .config/nvim-v2/lua/plugins/sleuth.lua
@@ -1,5 +1,5 @@
 -- url: https://github.com/tpope/vim-sleuth
 -- desc: automatically adjusts 'shiftwidth' and 'expandtab' heuristically based on the current file.
-return{
-  "https://github.com/tpope/vim-sleuth"
+return {
+	"https://github.com/tpope/vim-sleuth",
 }
