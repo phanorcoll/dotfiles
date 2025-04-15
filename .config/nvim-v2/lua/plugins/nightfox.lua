ae5cbd017eeb5f7c11796181257dec9c541fd642
Phanor Coll <phanorcoll@gmail.com>
2025-04-15 11:13:44 -0400

    chore(nightfox): change theme

---
+++ .config/nvim-v2/lua/plugins/nightfox.lua
@@ -0,0 +1,23 @@
+-- url: https://github.com/EdenEast/nightfox.nvim
+-- desc: A highly customizable theme for vim and neovim with support for lsp, treesitter and a variety of plugins.
+return {
+	"EdenEast/nightfox.nvim",
+	config = function()
+		require("nightfox").setup({
+			options = {
+				transparent = true,
+				styles = {
+					comments = "italic",
+					keywords = "bold",
+					functions = "italic,bold",
+					variables = "italic",
+					types = "italic",
+				},
+			},
+			palettes = {},
+			specs = {},
+			highlights = {},
+		})
+		vim.cmd("colorscheme nordfox")
+	end,
+}
