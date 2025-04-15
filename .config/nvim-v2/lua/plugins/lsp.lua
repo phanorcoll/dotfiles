1a0bbb8b9b2ec0e52c1d8317a905ef2b6297e55f
Phanor Coll <phanorcoll@gmail.com>
2025-04-15 10:53:46 -0400

    fix(lsp): issue with diagnostic icons

--- .config/nvim-v2/lua/plugins/lsp.lua
+++ .config/nvim-v2/lua/plugins/lsp.lua
@@ -139,30 +139,47 @@ return {
 
 		-- Diagnostic Config
 		-- See :help vim.diagnostic.Opts
+		-- remove this block for Neovim 0.12
+		local signs = {
+			{ name = "DiagnosticSignError", text = "󰅚 " },
+			{ name = "DiagnosticSignWarn", text = "󰀪 " },
+			{ name = "DiagnosticSignInfo", text = "󰋽 " },
+			{ name = "DiagnosticSignHint", text = "󰌶 " },
+		}
+
+		for _, sign in ipairs(signs) do
+			vim.fn.sign_define(sign.name, { text = sign.text, texthl = sign.name, numhl = "" })
+		end
 		vim.diagnostic.config({
+			signs = true,
 			severity_sort = true,
 			float = { border = "rounded", source = "if_many" },
 			underline = { severity = vim.diagnostic.severity.ERROR },
-			signs = {
-				text = {
-					[vim.diagnostic.severity.ERROR] = "󰅚 ",
-					[vim.diagnostic.severity.WARN] = "󰀪 ",
-					[vim.diagnostic.severity.INFO] = "󰋽 ",
-					[vim.diagnostic.severity.HINT] = "󰌶 ",
-				},
-			},
+			-- enable this for 0.12, right now is not available
+			-- signs = {
+			-- 	text = {
+			-- 		[vim.diagnostic.severity.ERROR] = "󰅚 ",
+			-- 		[vim.diagnostic.severity.WARN] = "󰀪 ",
+			-- 		[vim.diagnostic.severity.INFO] = "󰋽 ",
+			-- 		[vim.diagnostic.severity.HINT] = "󰌶 ",
+			-- 	},
+			-- },
 			virtual_text = {
 				source = "if_many",
 				spacing = 2,
 				format = function(diagnostic)
-					local diagnostic_message = {
-						[vim.diagnostic.severity.ERROR] = diagnostic.message,
-						[vim.diagnostic.severity.WARN] = diagnostic.message,
-						[vim.diagnostic.severity.INFO] = diagnostic.message,
-						[vim.diagnostic.severity.HINT] = diagnostic.message,
-					}
-					return diagnostic_message[diagnostic.severity]
+					return diagnostic.message
 				end,
+				-- enable this for 0.12, right now is not available
+				-- format = function(diagnostic)
+				-- 	local diagnostic_message = {
+				-- 		[vim.diagnostic.severity.ERROR] = diagnostic.message,
+				-- 		[vim.diagnostic.severity.WARN] = diagnostic.message,
+				-- 		[vim.diagnostic.severity.INFO] = diagnostic.message,
+				-- 		[vim.diagnostic.severity.HINT] = diagnostic.message,
+				-- 	}
+				-- 	return diagnostic_message[diagnostic.severity]
+				-- end,
 			},
 		})
 
