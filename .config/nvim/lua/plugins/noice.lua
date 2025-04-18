145355dac206948ac5a5bc57c810b08d876adbe3
Phanor Coll <phanorcoll@gmail.com>
2025-04-18 11:38:03 -0400

    chore(noice): change notify behavior. - move noice configs to the opts table.

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -5,17 +5,52 @@ return {
 	event = "VeryLazy",
 	opts = {
 		-- add any options here
+		cmdline = {
+			view = "cmdline_popup",
+			opts = {
+				position = {
+					row = "50%",
+					col = "50%",
+				},
+				size = {
+					height = "auto",
+				},
+			},
+		},
+		lsp = {
+			progress = {
+				enabled = false,
+			},
+			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+			},
+		},
+		-- you can enable a preset for easier configuration
+		presets = {
+			bottom_search = false, -- use a classic bottom cmdline for search
+			command_palette = true, -- position the cmdline and popupmenu together
+			long_message_to_split = true, -- long messages will be sent to a split
+			inc_rename = false, -- enables an input dialog for inc-rename.nvim
+			lsp_doc_border = false, -- add a border to hover docs and signature help
+		},
 	},
 	keys = {
 		{
 			"<leader>nh",
 			"<CMD>NoiceHistory<CR>",
-			desc = "View Noice History",
+			desc = "View Noice history",
 		},
 		{
 			"<leader>ne",
 			"<CMD>NoiceErrors<CR>",
-			desc = "View Noice Errors",
+			desc = "View Noice errors",
+		},
+		{
+			"<leader>nl",
+			"<CMD>NoiceLast<CR>",
+			desc = "View last message",
 		},
 	},
 	dependencies = {
@@ -24,41 +59,13 @@ return {
 		-- OPTIONAL:
 		--   `nvim-notify` is only needed, if you want to use the notification view.
 		--   If not available, we use `mini` as the fallback
-		"rcarriga/nvim-notify",
+		{
+			"rcarriga/nvim-notify",
+			opts = {
+				timeout = 1000, -- 1 second
+				stages = "fade", -- fade animation
+				-- other nvim-notify specific options...
+			},
+		},
 	},
-	config = function()
-		require("noice").setup({
-			cmdline = {
-				view = "cmdline_popup",
-				opts = {
-					position = {
-						row = "50%",
-						col = "50%",
-					},
-					size = {
-						height = "auto",
-					},
-				},
-			},
-			lsp = {
-				progress = {
-					enabled = false,
-				},
-				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-				},
-			},
-			-- you can enable a preset for easier configuration
-			presets = {
-				bottom_search = false, -- use a classic bottom cmdline for search
-				command_palette = true, -- position the cmdline and popupmenu together
-				long_message_to_split = true, -- long messages will be sent to a split
-				inc_rename = false, -- enables an input dialog for inc-rename.nvim
-				lsp_doc_border = false, -- add a border to hover docs and signature help
-			},
-		})
-	end,
 }
