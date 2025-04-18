a6b89da998350bebc3347f23905a6e0a20a7fc37
Phanor Coll <phanorcoll@gmail.com>
2025-04-18 10:58:04 -0400

    chore(noice): enable nvim-notify and change position of input. - Add new keymaps for NoiceHistory and NoiceErrors.

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -6,20 +6,44 @@ return {
 	opts = {
 		-- add any options here
 	},
+	keys = {
+		{
+			"<leader>nh",
+			"<CMD>NoiceHistory<CR>",
+			desc = "View Noice History",
+		},
+		{
+			"<leader>ne",
+			"<CMD>NoiceErrors<CR>",
+			desc = "View Noice Errors",
+		},
+	},
 	dependencies = {
 		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 		"MunifTanjim/nui.nvim",
 		-- OPTIONAL:
 		--   `nvim-notify` is only needed, if you want to use the notification view.
 		--   If not available, we use `mini` as the fallback
-		-- "rcarriga/nvim-notify",
+		"rcarriga/nvim-notify",
 	},
 	config = function()
 		require("noice").setup({
 			cmdline = {
-				view = "cmdline",
+				view = "cmdline_popup",
+				opts = {
+					position = {
+						row = "50%",
+						col = "50%",
+					},
+					size = {
+						height = "auto",
+					},
+				},
 			},
 			lsp = {
+				progress = {
+					enabled = false,
+				},
 				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 				override = {
 					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
@@ -29,7 +53,7 @@ return {
 			},
 			-- you can enable a preset for easier configuration
 			presets = {
-				bottom_search = true, -- use a classic bottom cmdline for search
+				bottom_search = false, -- use a classic bottom cmdline for search
 				command_palette = true, -- position the cmdline and popupmenu together
 				long_message_to_split = true, -- long messages will be sent to a split
 				inc_rename = false, -- enables an input dialog for inc-rename.nvim
