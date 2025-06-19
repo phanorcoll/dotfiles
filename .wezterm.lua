3ac9e999b8b34a682957a8f9b68c877b1f9d01a2
Phanor Coll <phanorcoll@gmail.com>
2025-06-19 16:07:13 -0400

    feat(wezterm): add initial configuration file
    Add a new .wezterm.lua configuration file with custom settings for initial
    window size, font, color scheme, tab bar, window decorations, and opacity.
    This sets up a personalized terminal appearance and behavior.

---
+++ .wezterm.lua
@@ -0,0 +1,19 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.initial_cols = 190
+config.initial_rows = 40
+
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font_size = 15
+config.color_scheme = "Afterglow"
+
+config.enable_tab_bar = false
+config.window_decorations = "NONE"
+config.window_background_opacity = 0.96
+
+-- Finally, return the configuration to wezterm:
+return config
