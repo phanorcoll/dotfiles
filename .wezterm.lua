064ed16a2ddda0ef03c239cbad6520ad515d99e6
Phanor Coll <phanorcoll@gmail.com>
2025-07-27 18:48:10 -0400

    feat(tmux,wezterm): add tmux popups & enable tab bar
    - Added several tmux key bindings for popups to manage sessions and run
      custom commands, including full-screen and session management popups.
    - Removed weather from dracula plugins and related location settings.
    - Enabled tab bar in wezterm for better session navigation.

--- .wezterm.lua
+++ .wezterm.lua
@@ -11,7 +11,7 @@ config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 15
 config.color_scheme = "Afterglow"
 
-config.enable_tab_bar = false
+config.enable_tab_bar = true
 config.window_decorations = "NONE"
 config.window_background_opacity = 0.96
 
